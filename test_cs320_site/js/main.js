c84683675952da02ce5dc46906f328793184a200
KSilverman <ksilverman@umass.edu>
2018-10-18 17:45:07 -0400

    Getting rid of test_cs320_site folder

--- test_cs320_site/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).on("dblclick", function(event)
-{
-  event.preventDefault();
-  var target = $(event.target);
-  if (target.parent('div#flexbox').length) 
-  {
-    alert('Double clicked a box');
-  }
-});
