37776b86fe468403d4a43b0739d8542190f3f627
samjharris <sjharris@umass.edu>
2018-10-18 19:32:29 -0400

    cleanup

--- backend/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-#__name__ is a variable passed to the class 'Flask', which is a
-#python specific predefined variable, which is set to
-#the name of the module used, and functions as a 'starting point'
-app = Flask(__name__)
-
-from app import routes
