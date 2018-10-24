8236d8160848a761875c57e84134eafbe48f3ea7
KSilverman <ksilverman@umass.edu>
2018-10-24 12:54:24 -0400

    comitting vue example

---
+++ vue/run.py
@@ -0,0 +1,17 @@
+from flask import Flask, flash, render_template
+
+class VueFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        variable_start_string='((',
+        variable_end_string='))'
+    ))
+
+app = VueFlask(__name__)
+
+@app.route('/')
+def test():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   	app.run(host='0.0.0.0', port=80, debug=True)
