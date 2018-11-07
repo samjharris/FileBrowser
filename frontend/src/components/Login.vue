c98471d8993acf4ff109d608876a7437441809fa
KSilverman <ksilverman@umass.edu>
2018-11-07 14:25:22 -0500

    Adding intial frontend structure with Bootstrap and Font-Awesome dependencies

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>{{ place }}</h1>
+	
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    place: String
+  }
+}
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHdd } from '@fortawesome/free-regular-svg-icons'
+
+
+library.add(faFileDownload, faHdd)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
