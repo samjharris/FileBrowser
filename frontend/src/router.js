526aea27f85e3263f1e6c08d56d8347051bed968
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2018-11-10 17:47:59 -0500

    routes set up

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,21 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from './components/HelloWorld'
+import Login from './components/Login'
 import Assets from './components/Assets'
+import error404 from './components/error404'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/assets',
       name: 'Assets',
       component: Assets
     },
     {
-      path: '/assets',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      name:'Error404',
+      component: error404
     }
   ]
 })
